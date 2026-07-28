deef77191e6baa041f91ed1d6a635bb64b0d0738
lothrond <lothrond@proton.me>
2026-07-28 12:04:01 -0400

    Stop skipping the preseed.

--- console.sh
+++ console.sh
@@ -6,4 +6,8 @@ make build \
     SESSION_TYPE="x11" \
     BLURAY_SUPPORT="true" \
     NATIVE_STEAM="true" \
-    BROWSER="firefox"
+    BROWSER="chrome" \
+    WIFI_SSID="MyHomeNetwork" \
+    WIFI_PASS="SuperSecretPassword" \
+    ROOT_PASSWORD="rootpassword123" \
+    USER_PASSWORD="userpassword123"
--- desktop.sh
+++ desktop.sh
@@ -6,4 +6,7 @@ make build \
     SESSION_TYPE="wayland" \
     BLURAY_SUPPORT="false" \
     NATIVE_STEAM="false" \
-    BROWSER="firefox"
+    BROWSER="firefox" \
+    WIFI_SSID="MyHomeNetwork" \
+    WIFI_PASS="SuperSecretPassword" \
+    ROOT_PASSWORD="rootpassword123"
